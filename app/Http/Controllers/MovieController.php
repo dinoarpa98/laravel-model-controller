85f8f6eb9c3421b492bb4f5bb2bfcb5cce01ce8c
dinoarpa98 <dinoarpa98@gmail.com>
2022-05-27 09:55:44 +0200

    end

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Movie;
+
+class MovieController extends Controller
+{
+    public function index(){
+
+        $movies = Movie::all();
+
+        return view('home', compact('movies'));
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- importare i file css --}}
+    <link rel="stylesheet" href="  {{ asset('css/app.css')  }} ">
+    <title>Home</title>
+</head>
+<body>
+
+    <h2 class="text-primary">Home</h2>
+
+    @forelse ($movies as $movie)
+    <div>
+        <div class="col card">
+            <div class="card-body">
+                <h5 class="card-title"> {{ $movie -> title  }}</h5>
+
+            </div>
+        </div>
+    </div>        
+    @empty
+
+    <p> empty </p>
+        
+    @endforelse
+
+    {{-- import javascript --}}
+    <script src=" {{ asset('js/app.js')  }} "></script>
+    
+</body>
+</html>
